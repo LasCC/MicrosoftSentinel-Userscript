c06af61372b591c9d4dfef7479e0c66caabf19a7
Ludovic COULON <coulonludovicc@gmail.com>
2026-03-06 11:29:23 -0500

    fix: use regex replace for ES2020 lib compatibility

--- src/ui/query-list.ts
+++ src/ui/query-list.ts
@@ -475,9 +475,9 @@ function createChevronSvg(): SVGSVGElement {
 
 function escapeHtml(value: string): string {
   return value
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#39;');
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 }
